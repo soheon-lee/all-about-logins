bf2c48173e7e27b3a0af62122ebcbfc047a3c21b
soheon-lee <lsheon93@gmail.com>
2020-07-20 21:27:17 +0900

    Add: 토큰 만료 시간 설정

--- account/views.py
+++ account/views.py
@@ -2,6 +2,11 @@ import json
 import bcrypt
 import jwt
 
+from datetime import (
+    datetime,
+    timedelta
+)
+
 from django.views           import View
 from django.http            import HttpResponse
 from django.http            import JsonResponse
@@ -10,71 +15,66 @@ from django.core.validators import (
     ValidationError
 )
 
-from local_settings import (
+from config  import (
     SECRET_KEY,
     ALGORITHM
 )
-from .models        import Account
+from .models import Account
 
 class SignUpView(View):
     def post(self, request):
-        payload = json.loads(request.body)
+        payload  = json.loads(request.body)
+        email    = payload.get('email', None)
+        password = payload.get('password', None)
 
-        try:
-            validate_email(payload['email'])
-
+        if email and password:
             try:
+                validate_email(payload['email'])
+
                 if Account.objects.filter(email_account = payload['email']).exists():
                     return JsonResponse({'message': 'DUPLICATED_EMAIL'}, status = 400)
 
-                print("1) original password: ", payload['password'])
-                print("2) encoded password: ", payload['password'].encode())
-                print("3) bcrypt hashed with gensalt: ", bcrypt.hashpw(payload['password'].encode(), bcrypt.gensalt()))
-                print("4) decoded password: ", bcrypt.hashpw(payload['password'].encode(), bcrypt.gensalt()).decode())
-
                 password = bcrypt.hashpw(
-                    payload['password'].encode(), 
+                    payload['password'].encode(),
                     bcrypt.gensalt()
                 ).decode()
 
                 Account(
-                    name          = payload['name'],
                     email_account = payload['email'],
                     password      = password
                 ).save()
 
-                return HttpResponse(status = 200)
+                return JsonResponse({'message': 'SUCCESS'}, status = 200)
 
-            except KeyError as e:
-                return JsonResponse({'message': f'MISSING_{e}'}, status = 400)
+            except ValidationError:
+                return JsonResponse({'message': 'INVALID_EMAIL'}, status = 400)
 
-        except ValidationError:
-            return JsonResponse({'message': 'INVALID_EMAIL'}, status = 400)
+        return JsonResponse({'message': 'KEY_ERROR'}, status = 400)
 
 class SignInView(View):
     def post(self, request):
         payload  = json.loads(request.body)
-        email    = payload['email']
-        password = payload['password']
+        email    = payload.get('email', None)
+        password = payload.get('password', None)
 
-        try:
-            validate_email(email)
-
+        if email and password:
             try:
                 if Account.objects.filter(email_account = email).exists():
                     user = Account.objects.get(email_account = email)
 
                     if bcrypt.checkpw(password.encode(), user.password.encode()):
-                        token = jwt.encode({'user_email':email}, SECRET_KEY, algorithm = ALGORITHM).decode()
+                        exp   = datetime.utcnow() + timedelta(hours = 1)
+                        token = jwt.encode(
+                            {
+                                'user_id' : user.id,
+                                'exp'     : exp
+                            }, SECRET_KEY, algorithm = ALGORITHM).decode()
+
                         return JsonResponse({'Authorization': token}, status = 200)
 
-                    return HttpResponse(status = 401)
+                    return JsonResponse({'message': 'UNAUTHORIZED'}, status = 401)
 
-                return HttpResponse(status = 401)
+                return JsonResponse({'message': 'UNAUTHORIZED'}, status = 401)
 
-            except KeyError as e:
-                return JsonResponse({'message': f'MISSING_{e}'}, status = 400)
-
-        except ValidationError:
-            return JsonResponse({'message': 'INVALID_EMAIL'}, status = 400)
+        return JsonResponse({'message': 'KEY_ERROR'}, status = 400)
 
