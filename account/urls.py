19b6ef56f8a3dc66885a9d14fc78095c7970a0c4
soheon-lee <lsheon93@gmail.com>
2020-09-04 15:46:48 +0900

    add DogView

--- account/urls.py
+++ account/urls.py
@@ -3,9 +3,15 @@ from django.urls import path
 from .views import (
     SignUpView,
     SignInView,
+    KakaoLoginView,
+    GoogleLoginView,
+    DogView,
 )
 
 urlpatterns = [
-   path('', SignUpView.as_view()),
-   path('/login', SignInView.as_view()),
+    path('', SignUpView.as_view()),
+    path('/login', SignInView.as_view()),
+    path('/kakao', KakaoLoginView.as_view()),
+    path('/google', GoogleLoginView.as_view()),
+    path('/dogs', DogView.as_view()),
 ]
--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
+import jwt
 import json
 import bcrypt
-import jwt
+import requests
 
 from datetime import (
     datetime,
@@ -19,9 +20,20 @@ from config  import (
     SECRET_KEY,
     ALGORITHM
 )
-from .models import Account
+from .models import (
+    Account,
+    SocialMedia,
+    Dog
+)
 
 class SignUpView(View):
+    def get(self, request):
+        users = Account.objects.all()
+        user_data = [{
+            'email': user.email_account
+        } for user in users]
+        return JsonResponse({'users': user_data}, status = 200)
+
     def post(self, request):
         payload  = json.loads(request.body)
         email    = payload.get('email', None)
@@ -49,7 +61,13 @@ class SignUpView(View):
             except ValidationError:
                 return JsonResponse({'message': 'INVALID_EMAIL'}, status = 400)
 
-        return JsonResponse({'message': 'KEY_ERROR'}, status = 400)
+        missing_key = (
+            '\'email\'' * (not email) +
+            ' and ' * ((not email) * (not password))+
+            '\'password\'' * (not password)
+        )
+
+        return JsonResponse({'message': 'KEY_ERROR: ' + missing_key}, status = 400)
 
 class SignInView(View):
     def post(self, request):
@@ -58,23 +76,113 @@ class SignInView(View):
         password = payload.get('password', None)
 
         if email and password:
-            try:
-                if Account.objects.filter(email_account = email).exists():
-                    user = Account.objects.get(email_account = email)
+            if Account.objects.filter(email_account = email).exists():
+                user = Account.objects.get(email_account = email)
 
-                    if bcrypt.checkpw(password.encode(), user.password.encode()):
-                        exp   = datetime.utcnow() + timedelta(hours = 1)
-                        token = jwt.encode(
-                            {
-                                'user_id' : user.id,
-                                'exp'     : exp
-                            }, SECRET_KEY, algorithm = ALGORITHM).decode()
+                if bcrypt.checkpw(password.encode(), user.password.encode()):
+                    exp   = datetime.utcnow() + timedelta(hours = 1)
+                    token = jwt.encode(
+                        {
+                            'user_id' : user.id,
+                            'exp'     : exp
+                        }, SECRET_KEY, algorithm = ALGORITHM).decode()
 
-                        return JsonResponse({'Authorization': token}, status = 200)
-
-                    return JsonResponse({'message': 'UNAUTHORIZED'}, status = 401)
+                    return JsonResponse({'Authorization': token}, status = 200)
 
                 return JsonResponse({'message': 'UNAUTHORIZED'}, status = 401)
 
+            return JsonResponse({'message': 'UNAUTHORIZED'}, status = 401)
+
         return JsonResponse({'message': 'KEY_ERROR'}, status = 400)
+
+class KakaoLoginView(View):
+    def post(self, request):
+
+        SOCIAL_MEDIA   = SocialMedia.objects.get(name = 'kakao')
+        KAKAO_AUTH_URL = 'https://kapi.kakao.com/v2/user/me'
+
+        try:
+            access_token   = request.headers.get('Authorization', None)
+            headers        = {'Authorization': f'Bearer {access_token}'}
+            kakao_response = requests.request("POST", kakao_url, headers = headers).json()
+            kakao_email    = kakao_response['kakao_account']['email']
+
+        except KeyError:
+            return JsonResponse({'message' : 'INVALID_KEYS'}, status = 400)
+
+        if Account.objects.filter(email_account = kakao_email, social_media = SOCIAL_MEDIA).exists():
+            user         = Account.objects.get(email_account = kakao_email, social_media = SOCIAL_MEDIA)
+            access_token = jwt.encode({'user_id' : user.id}, SECRET_KEY, ALGORITHM).decode('utf-8')
+
+            return JsonResponse({'access_token' : access_token}, status = 200)
+
+        user         = Account(email_account = kakao_email, social_media = SOCIAL_MEDIA).save()
+        access_token = jwt.encode({'user_id' : user.id}, SECRET_KEY, ALGORITHM).decode('utf-8')
+
+        return JsonResponse({'access_token' : access_token}, status = 200)
+
+class GoogleLoginView(View):
+    def post(self, request):
+        SOCIAL_MEDIA    = SocialMedia.objects.get(name = 'google')
+        GOOGLE_AUTH_URL = 'https://www.googleapis.com/oauth2/v3/tokeninfo?id_token='
+
+        access_token = request.headers.get('Authorization', None)
+        response     = requests.get(GOOGLE_AUTH_URL + access_token)
+        email        = response.json()['email']
+
+        if Account.objects.filter( email_account = email, social_media = SOCIAL_MEDIA).exists():
+
+            user = Account.objects.get(
+                email_account = email,
+                social_media  = SOCIAL_MEDIA
+            )
+            access_token = jwt.encode({'user_id' : user.id}, SECRET_KEY, ALGORITHM).decode('utf-8')
+
+            return JsonResponse({'access_token' : access_token}, status = 200)
+
+        user = Account(
+            email_account = email,
+            social_media  = SOCIAL_MEDIA
+        )
+        user.save()
+
+        access_token = jwt.encode({'user_id' : user.id}, SECRET_KEY, ALGORITHM).decode('utf-8')
+
+        return JsonResponse({'access_token' : access_token}, status = 200)
+
+
+class DogView(View):
+
+    def get(self, request):
+
+        doggy = Dog.objects.values()
+
+        return JsonResponse({'my_dogs' : list(doggy)}, status = 200)
+
+    def delete(self, request):
+
+        data = json.loads(request.body)
+        puppy_name = data['name']
+
+        Dog.objects.get(name = puppy_name).delete()
+
+        return JsonResponse({'message': 'DOG_DELTED!'}, status = 200)
+
+    def post(self, request):
+
+        data = json.loads(request.body)
+
+        puppy_name = data['name']
+        puppy_jong = data['jong']
+        puppy_age = data['dog_age']
+
+        Dog.objects.create(
+            name = puppy_name,
+            jong = puppy_jong,
+            age = puppy_age
+        )
+
+
+        return JsonResponse({'message': 'DOG_SAVED!'}, status = 201)
+
 
